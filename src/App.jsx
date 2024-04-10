55ec699088294118d273bbc533356d8408a68db5
Brayan Chavarro <brayanstik.21@outlook.com>
2024-04-10 18:44:11 -0500

    Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import React from "react";
+export function App() {
+  return (
+    <header className="bg-white shadow px-6">
+      <div>
+        <button>=</button>
+        <a href="#">logo</a>
+        <div>
+          <button>theme</button>
+          <button>user</button>
+        </div>
+      </div>
+    </header>
+  );
+}
